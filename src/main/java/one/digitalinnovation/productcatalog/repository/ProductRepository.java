36e0f45273832f870020680b7ab777f78e205900
Tauan Mateus[git config --global user.name tauan <tauanmateus1002@gmail.com>
2021-09-08 03:44:17 -0300

    microservicos

---
+++ src/main/java/one/digitalinnovation/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.productcatalog.repository;
+
+import one.digitalinnovation.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+
+}
